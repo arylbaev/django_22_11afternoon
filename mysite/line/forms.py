8430ed32f1c3bb3f805d2e628e58588f344c6c7b
vaarylbaev <vadim.zq0@gmail.com>
2023-12-06 11:52:17 +0300

    6.12 afternoon

--- mysite/line/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from models import Comment
-
-
-class AddCommentForm(forms.Form):
-    text = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
-    #author =
